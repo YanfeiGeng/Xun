8a0fa7465b595d165e1a678d2fd8219edf4dc5ec
Jay Xia <notoxxia@gmail.com>
2013-01-26 15:26:24 +0800

    modify the main scene

--- xun/xun.js
+++ xun/xun.js
@@ -1,13 +1,13 @@
 //set main namespace
 goog.provide('xun');
 
-
 //get requirements
 goog.require('lime.Director');
 goog.require('lime.Scene');
 goog.require('lime.Layer');
 goog.require('lime.Circle');
 goog.require('lime.Label');
+goog.require('lime.GlossyButton');
 goog.require('lime.animation.Spawn');
 goog.require('lime.animation.FadeTo');
 goog.require('lime.animation.ScaleTo');
@@ -15,60 +15,40 @@ goog.require('lime.animation.MoveTo');
 
 
 // entrypoint
-xun.start = function(){
+xun.start = function() {
 
-	var director = new lime.Director(document.body,1024,768),
-	    scene = new lime.Scene(),
+	xun.director = new lime.Director(document.body, 320, 460);
+	var scene = new lime.Scene(),
+	    layer = new lime.Layer();
 
-	    target = new lime.Layer().setPosition(512,384),
-        circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
-        lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
-        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
-            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
+	//var background = new lime.Sprite().setFill(255,150,0).setSize(1536, 2048);
+	//layer.appendChild(background);	
+		
+	var btn = new lime.GlossyButton("Play").setSize(100,50).setPosition(160, 200);
+	goog.events.listen(btn, 'click', function() {
+			xun.newgame(1);
+	});
+	layer.appendChild(btn);
+	
+	scene.appendChild(layer);
 
-
-    //add circle and label to target object
-    target.appendChild(circle);
-    target.appendChild(lbl);
-
-    //add target and title to the scene
-    scene.appendChild(target);
-    scene.appendChild(title);
-
-	director.makeMobileWebAppCapable();
-
-    //add some interaction
-    goog.events.listen(target,['mousedown','touchstart'],function(e){
-
-        //animate
-        target.runAction(new lime.animation.Spawn(
-            new lime.animation.FadeTo(.5).setDuration(.2),
-            new lime.animation.ScaleTo(1.5).setDuration(.8)
-        ));
-
-        title.runAction(new lime.animation.FadeTo(1));
-
-        //let target follow the mouse/finger
-        e.startDrag();
-
-        //listen for end event
-        e.swallow(['mouseup','touchend'],function(){
-            target.runAction(new lime.animation.Spawn(
-                new lime.animation.FadeTo(1),
-                new lime.animation.ScaleTo(1),
-                new lime.animation.MoveTo(512,384)
-            ));
-
-            title.runAction(new lime.animation.FadeTo(0));
-        });
-
-
-    });
-
+	xun.director.makeMobileWebAppCapable();
 	// set current scene active
-	director.replaceScene(scene);
+	xun.director.replaceScene(scene);
 
-}
+};
+
+xun.newgame = function(mode) {
+	var scene = new lime.Scene(),
+		layer = new lime.Layer();
+
+	scene.appendChild(layer);
+
+	//var game = new xun.Game(mode);
+	//layer.appendChild(game);	
+
+	xun.director.replaceScene(scene);
+};
 
 
 //this is required for outside access after code is compiled in ADVANCED_COMPILATIONS mode
