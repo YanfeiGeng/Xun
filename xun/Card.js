8951d83aa16111d85c95e7c47b6af771723d7d07
Jay Xia <notoxxia@gmail.com>
2013-01-27 11:32:29 +0800

    modify card

--- xun/Card.js
+++ xun/Card.js
@@ -4,12 +4,12 @@ goog.require('lime.Label');
 
 xun.Card = function() {
 	lime.Sprite.call(this);
-	this.life = 100; //number, full is 1 
-	this.attack = 100; //number, depends on different level
+	this.life = 60; //number, full is 1 
+	this.attack = 3; //number, depends on different level
 	this.exp = 0; //number, Will increase along with attack monster
 	this.level = 0; //Number, related with exp
 	
-	this.refresh();
+	this.init();
 };
 goog.inherits(xun.Card, lime.Sprite);
 
@@ -58,13 +58,16 @@ xun.Card.prototype.getLevel = function(){
 	return this.level;
 };
 
-xun.Card.prototype.refresh = function(){
-	this.removeAllChildren();
-	
-	this.lifeLabel = new lime.Label(this.life).setSize(100, 100).setPosition(100, 10).setAnchorPoint(0, 0);
-	this.attackLabel = new lime.Label(this.attack).setSize(100, 100).setPosition(50, 10).setAnchorPoint(0, 0);
-	this.expLabel = new lime.Label(this.exp).setSize(100, 100).setPosition(20, 10).setAnchorPoint(0, 0);
-	this.card = new lime.Sprite().setFill('assets/player01.png').setPosition(634, 24).setAnchorPoint(0, 0);
+xun.Card.prototype.init = function(){
+	var layer = new lime.Layer();
+	var card = new lime.Sprite().setFill('assets/player01.png').setPosition(0, 0).setAnchorPoint(0, 0);
+	this.lifeLabel = new lime.Label(this.life).setPosition(80, 250).setAnchorPoint(0, 0)
+		.setFontColor("#DDDDDD").setFontSize(40);
+	this.attackLabel = new lime.Label(this.attack).setPosition(194, 250).setAnchorPoint(0, 0)
+		.setFontColor("#DDDDDD").setFontSize(40);
+	var expbg = new lime.Sprite().setFill("#fff").setSize(59, 59).setPosition(17, 20).setAnchorPoint(0, 0);
+	this.expLabel = new lime.Label(this.exp).setPosition(34, 25).setAnchorPoint(0, 0)
+		.setFontColor("#DDDDDD").setFontSize(40);
 	//this.otherCard = new lime.
 	
 	var spriteCreater = new xun.H_Spirite();
@@ -77,10 +80,20 @@ xun.Card.prototype.refresh = function(){
 		y : 50
 	};
 	//this.appendChild(spriteCreater.Create('card',position, scale));
-	this.appendChild(this.card);
-	this.appendChild(this.lifeLabel);
-	this.appendChild(this.attackLabel);
-	this.appendChild(this.expLabel);	
+	layer.appendChild(card);
+	layer.appendChild(this.lifeLabel);
+	layer.appendChild(this.attackLabel);
+	//layer.appendChild(expbg);
+	layer.appendChild(this.expLabel);	
 		
+	layer.setPosition(634, 24).setAnchorPoint(0, 0);
+	
+	this.appendChild(layer);
 	this.setPosition(0, 1704).setAnchorPoint(0, 0).setFill('assets/mianban02.png');
+};
+
+xun.Card.prototype.refresh = function(){
+	this.lifeLabel.setText(this.life);
+	this.attackLabel.setText(this.attack);
+	this.expLabel.setText(this.exp);
 };
