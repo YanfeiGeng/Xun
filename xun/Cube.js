ba4eaba4cb52ef6c21f38679d576b92cc30f7911
Jay Xia <notoxxia@gmail.com>
2013-01-30 07:46:03 +0800

    完成cube最基本的两个动作: 1. 移掉遮罩； 2. 打开箱子。

--- xun/Cube.js
+++ xun/Cube.js
@@ -1,10 +1,30 @@
 goog.provide('xun.Cube');
 
-xun.Cube = function(type, cubeScale, posScale, rowCol) {
-	lime.Sprite.call(this);	
+xun.Cube = function(type) {
+	lime.Sprite.call(this);
 
+	this.type = type;
+	this.init();
 };
 goog.inherits(xun.Cube, lime.Sprite);
+
+xun.Cube.prototype.init = function(){
+	this.content = new lime.Sprite().setFill('assets/' + this.type + '.png').setPosition(0, 0).setAnchorPoint(0, 0);
+	this.box = new lime.Sprite().setFill('assets/block_yes.png').setPosition(0, 0).setAnchorPoint(0, 0);
+	this.mask = new lime.Sprite().setFill('assets/mask.png').setPosition(-23, -23).setAnchorPoint(0, 0);
+	
+	this.appendChild(this.content);
+	this.appendChild(this.box);
+	this.appendChild(this.mask);
+};
+
+xun.Cube.prototype.openBox = function(){
+	this.removeChild(this.box);
+};
+
+xun.Cube.prototype.removeMask = function(){
+	this.removeChild(this.mask);
+};
 
 xun.Cube.prototype.createCube = function(){
 	
--- xun/Puzzle.js
+++ xun/Puzzle.js
@@ -9,34 +9,25 @@ var randomPos = {
 
 xun.Puzzle = function() {
 	lime.Sprite.call(this);
-	this.name = '';
+
 	this.column = xun.Stage.config.puzzle.column;
 	this.row = xun.Stage.config.puzzle.row;
-	this.cubesType = xun.Stage.config.puzzle.cubes;
-	this.width = xun.Stage.config.puzzleWidth;
-	this.height = xun.Stage.config.puzzleHieght;
-	pos = 0;
+	
+	this.init();
+};
+goog.inherits(xun.Puzzle, lime.Sprite);
+
+xun.Puzzle.prototype.init = function(){
+	this.cubes = [];	
 	for(var x = 0; x < this.row; x++){
+		this.cubes[x] = [];
 		for(var y = 0; y < this.column; y++){
-			// var ref = this.cubesType[pos++]
-			var ref = 'mask';
-			var pos = {
-				row : x+1,
-				col : y+1
-			};
-			var cube = null;
-			if(x == randomPos.x && y == randomPos.y){
-				cube = new xun.Cube('enter', this.getCubeScale(), 
-				this.getPosScale(x, y), pos);
-			} else {
-				cube = new xun.Cube(ref, this.getCubeScale(), 
-				this.getPosScale(x, y), pos);
-			}
+			var	cube = new xun.Cube(); 
+			this.cubes[x][y] = cube;
+			
 			this.appendChild(cube);
 		}
 	}
-	//1. collect the cubes
-	this.cube = new xun.Cube();
 };
 
 xun.Puzzle.prototype.createPuzzle = function(){
